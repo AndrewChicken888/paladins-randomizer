63552e5fe7d8fa2a6f9ccff44dcf76910af2f1e2
AndrewChicken888 <andrewchicken888@gmail.com>
2021-07-22 21:15:08 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,17 @@
+Paladins Randomizer is a simple web application designed to give users a fun challenge by randomly generating every part of their Paladins experience. Users can randomize their champion, items, and even their loadout, all by simply pressing a few buttons.
+
+Outline:
+1. Allow users to randomize their champion. This should be on by default.
+
+2. Allow users to randomize their items. This should be off by default. All 4 items will be generated at once, not just 1 at a time.
+
+3. Allow users to randomize their loadouts. This should be off by default. The simplest way to do this is to create a graphic containing the 2 rows and columns of sets of 4 cards as shown in the loadout creation screen of each champion, then simply highlight the location of which cards the user should pick, and overlay the numbers over that. It should look something like this:
+
+_ _ 4 _     _ 2 1 _
+_ 3 _ _     _ 5 _ _
+
+4. Allow users to randomize all selected categories with the press of one button.
+
+5. Include links to social media at the footer of the page, to drive traffic to the AndrewChicken channels.
+
+6. Display all the previous points in a clean, aesthetically pleasing interface that is easy to navigate.
--- code.js
+++ code.js
@@ -5,51 +5,57 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
-//Cookies
-function setCookie(cname, cvalue) {
-	document.getElementById("newName").focus();
-	document.getElementById("newName").value = "";
-	document.getElementById("newCategory").value = "";
-	if (cname.indexOf("|") == 0 || cname.indexOf("|") == cname.length-1) {
-		document.getElementById("error").innerHTML = "Please fill in both boxes.";
-	} else {
-		  var d = new Date();
-		  d.setTime(d.getTime() + (1825*24*60*60*1000));
-		  var expires = "expires=" + d.toGMTString();
-		  document.cookie =  + "=" + cvalue + ";" + expires + ";path=/";
-	}
-}
+//Arrays containing the data for each section
+var champions = ["Androxus", "Ash", "Atlas", "Barik", "Bomb King", "Buck", "Cassie", "Corvus", "Dredge", "Drogoz", "Evie", "Fernando", "Furia", "Grohk", "Grover", "Imani", "Inara", "Io", "Jenos", "Khan", "Kinessa", "Koga", "Lex", "Lian", "Maeve", "Makoa", "Mal'Damba", "Moji", "Octavia", "Pip", "Raum", "Rei", "Ruckus", "Seris", "Sha Lin", "Skye", "Strix", "Talus", "Terminus", "Tiberius", "Torvald", "Tyra", "Vatu", "Viktor", "Vivian", "Vora", "Willo", "Yagorath", "Ying", "Zhin"];
+var items = ["Illuminate", "Resilience", "Guardian", "Haven", "Nimble", "Master Riding", "Morale Boost", "Chronos", "Rejuvenate", "Kill to Heal", "Life Rip", "Veteran", "Bulldozer", "Cauterize", "Deft Hands", "Wrecker"];
+var cards;
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
+var rand;
 
-//Delete a cookiefunction setCookie(cname,cvalue,exdays)
-function deleteCookie(cname) {
-  document.cookie = cname + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
-}
+/*
 
-function hideCookies() {
-	document.getElementById("cookies").style.height = "0px";
-}
+
+
+Note for the future!
+The way we could do the cards is have a picture of the card setup (2x2 sets of 4 cards), and make the ones which should be used highlighted in red, with the levels imprinted on top. Kind of like this
+
+_ _ 4 _     _ 2 1 _
+_ 3 _ _     _ 5 _ _
+
+
+
+
+
+*/
 
 //Run this on startup
 function init() {
 	//Test alert
 	//alert("This is working.");
-	setCookie("darkmode","true");
-	alert(getCookie("darkmode"));
 	
+	//Check if the user is on a mobile device and fix the graphics accordingly
+	if (!isMobileDevice()) {
+		//Put an event listener on the randomizer button so that it can be pressed on enter:
+		document.getElementById("randomize()").addEventListener("keyup", function(event) {
+			if (event.keyCode === 13) {
+				setCookie(document.getElementById('newName').value + '|' + document.getElementById('newCategory').value.toLowerCase());
+			}
+		});
+	}
+	
+	
+};
+
+//The big randomizer script
+function randomize() {
+	//Test alert
+	//alert("Randomizing!");
+	
+	//Check if the champion randomizer is toggled on:
+	if (document.getElementById("champToggle").checked) {
+		//alert("Champion Toggle is on");
+		rand = Math.round(Math.random()*champions.length)-1;
+		document.getElementById("champ_name").innerHTML = "Champion: " + champions[rand];
+		document.getElementById("champ_image").src = champions[rand] + ".png";
+	}
 };
